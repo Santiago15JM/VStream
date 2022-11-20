693188b1153003d655c616d54acd899cb0cd47b6
Santiago <santiago_jaramillo82201@elpoli.edu.co>
2022-11-19 23:10:51 -0500

    Deleted unused file and disabled debug

--- VStream/VStream/settings.py
+++ VStream/VStream/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0k8!5n-s3#rdcu#aqq%u-aou87^9t@6cj_t4dd58-hhf)dxo0l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
